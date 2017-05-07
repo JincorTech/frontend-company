af5e2727c4d20d995d886889e9fc7b54d323229d
David Totraev <totraev@gmail.com>
2017-05-07 13:20:55 +0300

    [DEV-121] description paragraph

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ const { API_HOST, API_PREFIX } = process.env
 
 export default {
   apiPrefix: API_PREFIX || '/api/v1',
-  apiHost: API_HOST || 'http://localhost'
+  apiHost: API_HOST || 'http://localhost:8080'
 }
