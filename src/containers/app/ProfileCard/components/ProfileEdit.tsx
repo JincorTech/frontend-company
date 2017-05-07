c219e0c9aee23afbf6d46a1e7b663b7bf94dcf60
Space Invader <amazing.space.invader@gmail.com>
2017-05-07 16:16:51 +0300

    [DEV-117] Initial values and validation in profile card

--- src/containers/app/ProfileCard/components/ProfileEdit.tsx
+++ src/containers/app/ProfileCard/components/ProfileEdit.tsx
@@ -3,7 +3,7 @@ import { Component } from 'react'
 import * as CSSModules from 'react-css-modules'
 import { reduxForm, Field, FormProps, SubmitHandler } from 'redux-form'
 
-import { initialValues, validate } from '../../../../helpers/common/profileCardEditProfile'
+import { required, minLength, maxLength } from '../../../../utils/validators'
 
 import Button from '../../../../components/common/Button'
 import RenderInput from '../../../../components/form/RenderInput'
@@ -13,9 +13,11 @@ import RenderImageUpload from '../../../../components/form/RenderImageUpload'
 export type Props = ComponentProps & FormProps<FormFields, ComponentProps, any>
 
 export type ComponentProps = {
+  onMount: () => void
   onSubmit: SubmitHandler<FormFields, ComponentProps, any>
   onCancel: () => void
   avatar: string
+  spinner: boolean
 }
 
 export type FormFields = {
@@ -26,8 +28,14 @@ export type FormFields = {
 }
 
 class ProfileEdit extends Component<Props, {}> {
+  public componentWillMount(): void {
+    const { onMount } = this.props
+
+    onMount()
+  }
+
   public render(): JSX.Element {
-    const { invalid, handleSubmit, onCancel, avatar } = this.props
+    const { invalid, handleSubmit, onCancel, avatar, spinner } = this.props
 
     const blackout = (
       <div style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0,
@@ -53,25 +61,36 @@ class ProfileEdit extends Component<Props, {}> {
 
         <Field
           component={RenderInput}
+          validate={[
+            required('Поле не может быть пустым'),
+            maxLength(15, 'Максимум 15 символов')
+          ]}
           name="firstName"
           type="text"
           placeholder="Имя"/>
 
         <Field
           component={RenderInput}
+          validate={[
+            required('Поле не может быть пустым'),
+            maxLength(15, 'Максимум 15 символов')
+          ]}
           name="lastName"
           type="text"
           placeholder="Фамилия"/>
 
         <Field
           component={RenderInput}
+          validate={[
+            maxLength(20, 'Максимум 20 символов')
+          ]}
           name="position"
           type="text"
           placeholder="Должность"/>
 
         <div styleName="form-buttons">
           <Button type="button" styleName="form-cancel-button" onClick={onCancel}>отменить</Button>
-          <Button type="submit" styleName="form-submit-button" disabled={invalid}>Сохранить</Button>
+          <Button type="submit" styleName="form-submit-button" disabled={invalid} spinner={spinner}>Сохранить</Button>
         </div>
       </form>
     )
@@ -80,7 +99,5 @@ class ProfileEdit extends Component<Props, {}> {
 
 const StyledComponent = CSSModules(ProfileEdit, require('../styles.css'))
 export default reduxForm<FormFields, ComponentProps>({
-  form: 'ProfileCardEdit',
-  initialValues,
-  validate
+  form: 'ProfileCardEdit'
 })(StyledComponent)
